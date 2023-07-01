6b5f53894ba2e31c07ae233a537ade5caa41df60
khushi sarangi <skhushisarangi@gmail.com>
2023-07-02 01:43:28 +0530

    first commit

--- Array.java
+++ Array.java
@@ -6,7 +6,22 @@ public class Array {
         marks[2]=10;
         marks[3]=9;
         marks[4]=10;
-        System.out.println(marks[4]);
+        //System.out.println(marks[4]);
+        //System.out.println(marks.length);
+        System.out.println("Printing using for loop");
+        for(int i=0;i<marks.length;i++){
+            System.out.println(marks[i]);
+        }   
+
+        System.out.println("Printing using for loop in reverse order");
+        for(int i=marks.length -1;i>=0;i--){
+            System.out.println(marks[i]);
+        }
+        
+        String [] student ={"harry", "rashi", "pihu"};
+        System.out.println(student[2]);
+       
+
 
     }
     
